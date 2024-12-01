a0aef5148e07f8466fdb202e13a69963a7c225eb
timotimmermans <151184563+timotimmermans@users.noreply.github.com>
2024-12-01 20:48:14 +0100

    change version grandparents

--- src/components/TenantPlatform.tsx
+++ src/components/TenantPlatform.tsx
@@ -54,7 +54,7 @@ const tenantAccounts: TenantAccount[] = [
     password: "topopaenoma",
     name: "Willy en Nellie",
     address: "Bernstraat 77, Oisterwijk",
-    referenceImages: ["/images/radiator_timo.png", "/images/window3_timo.png", "/images/tv_timo.png", "/images/seat_timo.png", "/images/painting2_timo.png"],
+    referenceImages: ["/images/door_1_timo.png", "/images/door_2_timo.png", "/images/radiator_2_timo.png", "/images/radiator_timo.png", "/images/door_3_timo.png"],
   },
   {
     email: "timmermans@gmail.com",
